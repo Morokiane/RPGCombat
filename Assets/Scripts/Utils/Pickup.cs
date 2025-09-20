6c4d7fdeac5ea9f473360db75d09080015fecbdf
MG <morokiane@gmail.com>
2025-09-20 15:56:33 -0600

    final

--- Assets/Scripts/Utils/Pickup.cs
+++ Assets/Scripts/Utils/Pickup.cs
@@ -1,16 +1,25 @@
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Utils {
     public class Pickup : MonoBehaviour {
         [SerializeField] private float pickupDistance;
-        [SerializeField] private float moveSpeed = 0.4f;
-        [SerializeField] private float accelRate = 0.2f;
-
+        [SerializeField] private float moveSpeed = 1f;
+        [SerializeField] private float accelRate = 0.5f;
+        [SerializeField] private float heightY = 2f;
+        [SerializeField] private float popDuration = 0.25f;
+        [SerializeField] private AnimationCurve animCurve;
+        
         private Vector2 moveDir;
         private Rigidbody2D rb;
 
         private void Awake() {
             rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start() {
+            StartCoroutine(SpawnAnim());
         }
 
         private void Update() {
@@ -32,6 +41,25 @@ namespace Utils {
         private void OnTriggerEnter2D(Collider2D _other) {
             if (_other.CompareTag("Player")) {
                 Destroy(gameObject);
+            }
+        }
+
+        private IEnumerator SpawnAnim() {
+            Vector2 startPoint = transform.position;
+            float randomX = transform.position.x + Random.Range(-2f, 2f);
+            float randomY = transform.position.y + Random.Range(-1f, 1f);
+            
+            Vector2 endPoint = new Vector2(randomX, randomY);
+            float timePassed = 0f;
+
+            while (timePassed < popDuration) {
+                timePassed += Time.deltaTime;
+                float linearT = timePassed / popDuration;
+                float heightT = animCurve.Evaluate(linearT);
+                float height = Mathf.Lerp(0f, heightY, heightT);
+                
+                transform.position = Vector2.Lerp(startPoint, endPoint, linearT) + new Vector2(0f, height);
+                yield return null;
             }
         }
     }
